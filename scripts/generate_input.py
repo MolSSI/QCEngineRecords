6e7e23d3c668e0ceecda1317fed55fcf5edcdf0e
Daniel Smith <malorian@me.com>
2019-02-21 18:07:58 -0500

    Scripts: update with dftd3 changes

--- scripts/generate_input.py
+++ scripts/generate_input.py
@@ -1,25 +1,48 @@
+import pprint
+import json
 import qcengine as qcng
 inp = {
     "molecule": {
         "geometry": [
-            0.0, 0.0, -0.1294769411935893, 0.0, -1.494187339479985,
-            1.0274465079245698, 0.0, 1.494187339479985, 1.0274465079245698
+            0.0,  0.0,               -0.1294769411935893,
+            0.0, -1.494187339479985,  1.0274465079245698,
+            0.0,  1.494187339479985,  1.0274465079245698
         ],
         "symbols": ["O", "H", "H"],
-        "connectivity": [[0, 1, 1], [0, 2, 1]]
     },
-    "driver": "energy",
+    "driver": "gradient",
     "keywords": {},
     "model": {
-        "method": "HF",
-        "basis": "sto-3g"
+        "method": "B3LYP-D3",
     }
 }
 
 import qcelemental as qcel
+from qcengine.programs import intf_dftd3
+
+def format_dict(d):
+    o = pprint.pformat(d, compact=True, indent=2, width=100)
+    o = o.replace("'", '"')
+    o = o.replace("True", "true")
+    o = o.replace("False", "false")
+    o = o.replace("None", "null")
+
+    return o
+
+print(format_dict(inp))
+print("\n------\n")
 
 inp = qcel.models.ResultInput(**inp)
-#print(inp)
+out = intf_dftd3.run_json(inp.json_dict())
+out["keywords"] = {}
 
-config = qcng.get_config()
-qcng.programs.molpro._format_input(inp, config)
+stdout = out.pop("stdout")
+print(stdout)
+print("\n------\n")
+
+out = qcel.models.Result(**out)
+print(format_dict(out.json_dict()))
+#
+#print(json.dumps(out.json_dict(), indent=2))
+
+
